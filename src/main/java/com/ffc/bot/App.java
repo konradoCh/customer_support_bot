60fc4e576cb034a6cce0c0afeeab70221c6f6e9e
konrad <konradFake@zajavka.pl>
2025-03-12 20:10:25 +0100

    next commit

--- src/main/java/com/ffc/bot/App.java
+++ src/main/java/com/ffc/bot/App.java
@@ -1,13 +1,9 @@
 package main.java.com.ffc.bot;
-
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( Bot.BOT_USERNAME );
+        System.out.println( Bot.BOT_TOKEN );
     }
 }
